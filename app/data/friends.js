be1875001817fefe5f05b8e12f48ac9b7f1b1623
AJohnson38 <asjohnson1213@gmail.com>
2018-09-14 19:39:02 -0400

    1

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends who've completed the .
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+    {
+      Name: "Saima",
+      Photo: "saima@example.com",
+      Q1: "000-000-0000",
+      Q2: "saimaCool",
+      Q3: "",
+      Q4: "",
+      Q5: "",
+      Q6: "",
+      Q7: "",
+      Q8: "",
+      Q9: "",
+      Q10: ""
+    } 
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
